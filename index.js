133484918f3306b7235801b392c2f0c4d54b5b8a
Fcortes92 <fercho92fcmail.com>
2025-05-06 09:02:14 -0500

    estructura basica proyecti express con conexion a base de datos

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
-const holaRouter = require("./routes/holaMundo.routes")
+const mongoose = require("mongoose")
 require("dotenv").config()
+
+const holaRouter = require("./routes/holaMundo.routes")
 const app = express()
 const PORT = process.env.PORT || 3006
 
@@ -11,6 +13,7 @@ app.get("/",(req,res)=>{
     res.send("hola mundo")
 })
 
+mongoose.connect(process.env.MONGO_URI).then(console.log("base de datos conectada")).catch(err=>console.error("no se pudo conectar a la BD".err))
 app.listen(PORT,()=>{
     console.log(`escuchando en el puerto${PORT}`)
 })
